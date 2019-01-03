5ba88b0600b54ffb1c43e4c10b3dcaa4b0037661
Grant Avery <grant.avery@spectrumhealth.org>
2019-01-03 16:37:49 -0500

    Fleshed out 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import timeywimey from '../images/wibbly-wobbly-timey-wimey-stuff.jpg'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
+    <p>You may have entered a time hole. Please try a different page.</p>
+    <img src={timeywimey} className="image-404"></img>
   </Layout>
 )
 
