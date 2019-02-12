864848456ef43362327c5114b074f50787decb8d
Grant Avery <grantavery@icloud.com>
2019-02-12 18:11:55 -0500

    Remove WordPress blog link

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,12 @@ const IndexPage = () => (
     <p>Hi, I’m a software developer currently attending Davenport University. I build websites and mobile apps for Spectrum Health and others, and am always learning new things.</p>
 
     <p>Feel free to check out the links below for other places you can find me:</p>
+  
+    <p><a href='https://github.com/GrantAvery'>GitHub</a></p>
 
     <p><a href='https://www.linkedin.com/in/grant-m-avery/'>LinkedIn</a></p>
 
-    <p><a href='https://github.com/GrantAvery'>GitHub</a></p>
-
     <p><a href='http://grant-avery.brandyourself.com/'>My BrandYourself profile</a></p>
-
-    <p>An old <a href='https://grantaveryblog.wordpress.com/'>WordPress blog</a> from high school</p>
   </Layout>
 )
 
