8ec8ebec212520315f3ffad949d11b29c1c93a6f
Grant Avery <grant.avery@spectrumhealth.org>
2019-02-12 21:23:02 -0500

    Created Links page

---
+++ src/pages/links.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const LinksPage = () => (
+  <Layout>
+    <SEO title="Links" />
+    <h1>Links</h1>
+    <div>
+      <p>Feel free to check out the links below for other places you can find me:</p>
+
+      <p><a href='https://github.com/GrantAvery/GrantAvery'>GitHub</a></p>
+
+      <p><a href='https://www.linkedin.com/in/grant-m-avery/'>LinkedIn</a></p>
+
+      <p><a href='http://grant-avery.brandyourself.com/'>My BrandYourself profile</a></p>
+
+      <p>
+        (It looks like these sites are all strong proponents of PascalCase&nbsp;
+        <span role="img" aria-label="smiley face">😃</span>
+        )
+      </p>
+    </div>
+  </Layout>
+)
+
+export default LinksPage
