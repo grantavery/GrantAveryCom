0ebc87660b51c62bbc39b7b9904a999f4abab2d0
Grant Avery <grantavery@icloud.com>
2020-10-27 21:09:59 -0400

    Moving Pi retrieval logic into the Node Pi API and abstracting it from here.

--- src/pages/pi.js
+++ src/pages/pi.js
@@ -7,10 +7,10 @@ const PiPage = () => (
   <Layout>
     <SEO title='3142 Digits of Pi' />
     <h1>3142 Digits of Pi</h1>
-    <p>This was simply a fun exercise with React components and API calls in GatsbyJS. 
-      It utilizes Google's <a href='https://pi.delivery/'>Pi API</a>, 
-      which unfortunately only allows requests of 1000 digits (or less) at a time. 
-      I opted to grab 3142 digits and call it good.</p>
+    <p>This was simply a fun exercise with using a React component to retrieve digits of Pi 
+      via a custom NodeJS API, which in turn gets its data from Google's <a href='https://pi.delivery/'>Pi API</a>. 
+      Google's API only allows requests for a maximum of 1000 digits at a time, so my Node API abstracts some logic to 
+      pull as many digits as I'd like, in this case 3142.</p>
     <div>
       <PiComponent></PiComponent>
     </div>
