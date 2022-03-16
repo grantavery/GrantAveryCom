73ca8451be9246f2d6d0529cda433a4e5d8a7a12
Grant Avery <grantavery@icloud.com>
2022-03-16 08:35:26 -0400

    Fix the Thanks page setup

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -4,14 +4,16 @@ import Seo from '../components/seo';
 import { Link } from 'gatsby';
 
 const Thanks = () => {
-  <Layout>
-    <div>
-     <Seo title='Thank you!' />
-      <h1>Thank you!</h1>
-      <p>Your message was successfully sent. I'll get back to you as soon as I can.</p>
-      <Link to='/'>Back to Home</Link>
-    </div>
-  </Layout>
+  return (
+    <Layout>
+      <div>
+      <Seo title='Thank you!' />
+        <h1>Thank you!</h1>
+        <p>Your message was successfully sent. I'll get back to you as soon as I can.</p>
+        <Link to='/'>Back to Home</Link>
+      </div>
+    </Layout>
+  );
 };
 
-export default Thanks;
+export default Thanks
